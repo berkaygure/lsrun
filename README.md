9d89e9e85d6c24706383fc7b6b96e8e29c1dace1
Berkay GÜRE <bgure@hotmail.com.tr>
2021-06-12 19:15:53 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# lsrun
+
+Lists scripts in package.json and runs selected script.
+
+
+## Usage 
+```javascript
+npx lsrun
+```
+
+## Screenshot
+![lsrun](./screenshot.png)
---
+++ index.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+'use strict';
+const fs = require("fs");
+const inquirer = require("inquirer");
+const { spawn } = require("child_process");
+
+const packageJsonLocation = process.cwd() + "/package.json";
+
+function runScript(script) {
+  spawn("npm", ["run", script], { stdio: "inherit" });
+}
+
+fs.readFile(packageJsonLocation, (err, data) => {
+  if (err) {
+    console.error("The package.json file not found");
+    process.exit();
+  }
+
+  const packageJson = JSON.parse(data);
+
+  if (!packageJson.scripts) {
+    console.error("The package.json do not have any scripts.");
+    process.exit();
+  }
+
+  const availableScripts = Object.keys(packageJson.scripts);
+
+  if (availableScripts.length === 1) {
+    console.log(`You have one script, Running: ${availableScripts[0]}`);
+    runScript(availableScripts[0]);
+  } else {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "script",
+          message: `You have ${availableScripts.length} scripts, Please select to run: `,
+          choices: availableScripts,
+        },
+      ])
+      .then(({ script }) => {
+        runScript(script);
+      });
+  }
+});
